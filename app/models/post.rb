4b005947d3db7e316a37228abeb276659fa0ce6a
hahm-d <dhahm01@gmail.com>
2020-09-19 11:29:11 -0400

    model updates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     belongs_to :poster, class_name: "User", foreign_key: "user_id", optional: true
     has_many :comments, dependent: :destroy
+    has_many_attached :images
 end
