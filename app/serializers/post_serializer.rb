61de5869e58b8d333ab03201f6a1b0f8022b1d72
hahm-d <dhahm01@gmail.com>
2020-09-22 19:39:49 -0400

    more seed data

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -3,10 +3,16 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :url, :views, :likes, :image
   has_one :user
 
-  def featured_image
+  def image
     if object.image.attached?
       {
-        url: rails_blob_url(object.image)
+        url: rails_blob_url(object.image, default_url_options[:host]),
+        cloudinary: object.image.service_url
+     }
+    else
+      {
+        url: "",
+        cloudinary: "",
       }
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,34 @@ Comment.destroy_all
 Relationship.destroy_all
 Type.destroy_all
 
-#users - normal users and instructors
-4.times do
-   User.create(
-        username: Faker::Name.name_with_middle,
+#Create Instructors
+    User.create(
+        username: "Raymee",
         email: Faker::Internet.free_email,
         password: "12345",
         status: "online",
-        description: "Yoga is the way of life. Follow me!"
+        description: "Yoga is the way of life. Follow me!",
+        twitter: "RayRay",
+        instagram: "Raymee",
         flag: true
-   ) 
+    ) 
+12.times do |i|
+  sample = User.create(
+            username: Faker::TvShows::RickAndMorty.character,
+            email: Faker::Internet.free_email,
+            password: "12345",
+            status: "online",
+            description: Faker::TvShows::RickAndMorty.quote,
+            twitter: Faker::Games::Pokemon.name,
+            instagram: Faker::Games::Overwatch.hero,
+            flag: true
+    ) 
+    sample.image.attach(io: File.open("/Users/daniel/Flatiron/personal/FitCo/FitCo-backend/samples/profile#{i+1}.png"), filename: "profile#{i+1}.png", content_type: 'image/png')
+
 end
 
 #Post by coaches/instructors
-3.times do
+4.times do
     Post.create(
         title: Faker::Coffee.blend_name,
         content: Faker::Coffee.notes,  
@@ -30,26 +44,29 @@ end
     )
 end
 
+#attach images
 User.first.image.attach(io: File.open('/Users/daniel/Flatiron/personal/FitCo/FitCo-backend/samples/raymeProfile.png'), filename: 'raymeProfile.png', content_type: 'image/png')
 Post.first.image.attach(io: File.open('/Users/daniel/Flatiron/personal/FitCo/FitCo-backend/samples/rayme1.png'), filename: 'rayme1.png', content_type: 'image/png')
-Post.first.image.attach(io: File.open('/Users/daniel/Flatiron/personal/FitCo/FitCo-backend/samples/rayme2.png'), filename: 'rayme2.png', content_type: 'image/png')
-Post.second.image.attach(io: File.open('/Users/daniel/Flatiron/personal/FitCo/FitCo-backend/samples/rayme3.png'), filename: 'rayme3.png', content_type: 'image/png')
-Post.third.image.attach(io: File.open('/Users/daniel/Flatiron/personal/FitCo/FitCo-backend/samples/rayme4.png'), filename: 'rayme4.png', content_type: 'image/png')
+Post.second.image.attach(io: File.open('/Users/daniel/Flatiron/personal/FitCo/FitCo-backend/samples/rayme2.png'), filename: 'rayme2.png', content_type: 'image/png')
+Post.third.image.attach(io: File.open('/Users/daniel/Flatiron/personal/FitCo/FitCo-backend/samples/rayme3.png'), filename: 'rayme3.png', content_type: 'image/png')
+Post.fourth.image.attach(io: File.open('/Users/daniel/Flatiron/personal/FitCo/FitCo-backend/samples/rayme4.png'), filename: 'rayme4.png', content_type: 'image/png')
 
-#comments that belong to user & post
-3.times do
+
+
+#comments
+User.all.each do |user|
     Comment.create(
-        comment: "wow! great post",
-        post: Post.all.last,
-        username: Post.all.last,
-        commenter_id: User.all.last.id
+        comment: Faker::Quote.yoda,
+        post: Post.all.first,
+        username: user.username,
+        commenter_id: user.id
     )
-end
+end 
 
 #type of fitness coach: boxing, yoga, hit, etc.
-type1 = Type.create(category: "Yoga", user_id: User.all.first.id )
-type2 = Type.create(category: "Boxing", user_id: User.all.first.id )
-type3 = Type.create(category: "Swimming", user_id: User.all.last.id )
+User.all.each do |user|
+    Type.create(category: "Hit Trainer", user_id: user.id )
+end
 
 
 #relationships 
